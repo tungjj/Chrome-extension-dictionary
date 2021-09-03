2ebaa8917521c518fde45aa990bd64615f05c1b2
tungjj <49489457+tungjj@users.noreply.github.com>
2021-09-03 22:00:21 +0700

    tu dien chi co nghia tieng viet

--- popup.js
+++ popup.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // process word from background
   var  {word}  = background;
   // console.log(word);
-  // textInput.innerHTML = word;
+  textInput.innerHTML = word;
   ////for oxford api
   const appId = "817d8cc8";
   const appKey = "af2a40e721acd4c13b2827a6919614f6";
@@ -45,11 +45,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-  sendRequestToGetPronunciationUK(checkValidWord, urlLemmas, appId, appKey);
-  checkValidWord.addEventListener("readystatechange", function(){
-    if (this.readyState === this.DONE) {
-        const outcome = JSON.parse(this.responseText);
-        console.log(outcome);
+  // sendRequestToGetPronunciationUK(checkValidWord, urlLemmas, appId, appKey);
+  // checkValidWord.addEventListener("readystatechange", function(){
+  //   if (this.readyState === this.DONE) {
+  //       const outcome = JSON.parse(this.responseText);
+  //       console.log(outcome);
         // if(outcome.error)
         // {
         //   console.log(outcome.error); // show err and nothing will be shown
@@ -66,60 +66,60 @@ document.addEventListener('DOMContentLoaded', function () {
         //   sendRequestToGetPronunciationUK(requestGetPronunciationUS, urlUS, appId, appKey);
         // }
        
-    }
-  });
-  sendRequestToWordAPI(requestToWordAPI, urlWordAPI);
-  requestToWordAPI.addEventListener("readystatechange", function(){
-    if (this.readyState === this.DONE) {
-      const outcome = JSON.parse(this.responseText);
-      console.log(outcome);
+  //   }
+  // });
+  // sendRequestToWordAPI(requestToWordAPI, urlWordAPI);
+  // requestToWordAPI.addEventListener("readystatechange", function(){
+  //   if (this.readyState === this.DONE) {
+  //     const outcome = JSON.parse(this.responseText);
+  //     console.log(outcome);
 
-      let meanings = [`${outcome.meaning.noun}`,
-                    `${outcome.meaning.verb}`,
-                    `${outcome.meaning.adjective}`,
-                    `${outcome.meaning.adverb}`
-      ];
-      meanings = meanings.join("\n");
-      definitionEn.innerHTML = meanings;
-    }
-  })
+  //     let meanings = [`${outcome.meaning.noun}`,
+  //                   `${outcome.meaning.verb}`,
+  //                   `${outcome.meaning.adjective}`,
+  //                   `${outcome.meaning.adverb}`
+  //     ];
+  //     meanings = meanings.join("\n");
+  //     definitionEn.innerHTML = meanings;
+  //   }
+  // })
 
-  sendRequestToGetPronunciationUK(requestGetPronunciationUK, urlUK, appId, appKey);
-  requestGetPronunciationUK.addEventListener("readystatechange", function () {
-    if (this.readyState === 4 ) {
-      const outcome = JSON.parse(this.responseText);
-      console.log(outcome);
+  // sendRequestToGetPronunciationUK(requestGetPronunciationUK, urlUK, appId, appKey);
+  // requestGetPronunciationUK.addEventListener("readystatechange", function () {
+  //   if (this.readyState === 4 ) {
+  //     const outcome = JSON.parse(this.responseText);
+  //     console.log(outcome);
 
       
-      // sendRequestToGetPronunciationUK(requestGetPronunciationUS, urlUS, appId, appKey);
+  //     // sendRequestToGetPronunciationUK(requestGetPronunciationUS, urlUS, appId, appKey);
 
-      const audioFileUK = new Audio(outcome.results[0].lexicalEntries[0].entries[0].pronunciations[0].audioFile);
-      const synonyms = outcome.results[0].lexicalEntries[0].entries[0].senses[0].synonyms;
+  //     const audioFileUK = new Audio(outcome.results[0].lexicalEntries[0].entries[0].pronunciations[0].audioFile);
+  //     const synonyms = outcome.results[0].lexicalEntries[0].entries[0].senses[0].synonyms;
       
-      audioUK.onclick = function () {
-        audioFileUK.play();
-      };
+  //     audioUK.onclick = function () {
+  //       audioFileUK.play();
+  //     };
 
-      // typeOfWord.innerHTML = `(${outcome.results[0].lexicalEntries[0].lexicalCategory.id})`;
-      pronunciationUK.innerHTML = `/${outcome.results[0].lexicalEntries[0].entries[0].pronunciations[0].phoneticSpelling}/`;
-      // definitionEn.innerHTML = ` ${outcome.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]}`;
-      synonym.innerHTML = synonyms.length == 0  ? "" : synonyms.map(e => e.text).join(', ');
-    }
-  })
-  sendRequestToGetPronunciationUK(requestGetPronunciationUS, urlUS, appId, appKey);
-  requestGetPronunciationUS.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-      const outcome = JSON.parse(this.responseText);
-      console.log(outcome);
-      const audioFileUS = new Audio(outcome.results[0].lexicalEntries[0].entries[0].pronunciations[1].audioFile);
+  //     // typeOfWord.innerHTML = `(${outcome.results[0].lexicalEntries[0].lexicalCategory.id})`;
+  //     pronunciationUK.innerHTML = `/${outcome.results[0].lexicalEntries[0].entries[0].pronunciations[0].phoneticSpelling}/`;
+  //     // definitionEn.innerHTML = ` ${outcome.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]}`;
+  //     synonym.innerHTML = synonyms.length == 0  ? "" : synonyms.map(e => e.text).join(', ');
+  //   }
+  // })
+  // sendRequestToGetPronunciationUK(requestGetPronunciationUS, urlUS, appId, appKey);
+  // requestGetPronunciationUS.addEventListener("readystatechange", function () {
+  //   if (this.readyState === 4) {
+  //     const outcome = JSON.parse(this.responseText);
+  //     console.log(outcome);
+  //     const audioFileUS = new Audio(outcome.results[0].lexicalEntries[0].entries[0].pronunciations[1].audioFile);
       
-      audioUS.onclick = function () {
-        audioFileUS.play();
-      };
-      pronunciationUS.innerHTML = `/${outcome.results[0].lexicalEntries[0].entries[0].pronunciations[1].phoneticSpelling}/`;
+  //     audioUS.onclick = function () {
+  //       audioFileUS.play();
+  //     };
+  //     pronunciationUS.innerHTML = `/${outcome.results[0].lexicalEntries[0].entries[0].pronunciations[1].phoneticSpelling}/`;
 
-    }
-  });
+  //   }
+  // });
 });
 
 function sendRequestToGetDefinition(xmlHttpRequest, keyWord) {
